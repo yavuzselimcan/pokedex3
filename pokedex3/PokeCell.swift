6c6792cda992825cbae8fb673d024985f70eed09
yavuzselimcan <yselim.can@gmail.com>
2017-06-03 00:41:27 +0300

    New changes

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Yavuz Selim Can on 02/06/2017.
+//  Copyright © 2017 Yavuz Selim Can. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Yavuz Selim Can on 01/06/2017.
+//  Copyright © 2017 Yavuz Selim Can. All rights reserved.
+//
+
+import Foundation
